71d426bc43548b18e77012e75b15a21cbdd1d094
Ziggie <michael.darren.springer@gmail.com>
2018-02-11 15:55:30 -0800

    fucking work

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    private int Level;
+
+    public void LoadLevels(string name)
+    
+    {
+        Debug.Log("level requested from Level Manager" + name);
+        SceneManager.LoadScene(name);
+    }
+	
+    public void QuitLevel()
+    {
+        Debug.Log("go ahead and quit losser ");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        // Application.LoadLevel(Application.loadedLevel + 1);
+        //LevelBuildNumber = SceneManager.GetSceneByBuildIndex();
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        
+        
+        
+    }
+
+   
+}
+
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    static MusicPlayer instance = null;
+
+
+    // Use this for initialization
+	void Awake()
+    {
+        //kills duplicate players
+		Debug.Log ("Music Player Awake" + GetInstanceID ());
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("dup MusicPlayer dead");
+        }
+
+        else
+        {   // keeps player alive
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+   
+	}
+}
